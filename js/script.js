ec5c8c8c40f2ec732fdaeaf6b6e9fbc1595bd3fd
Michele Lisignoli <michele@lisignoli.com>
2021-03-02 16:03:11 +0100

    array

--- js/script.js
+++ js/script.js
@@ -1,23 +1,16 @@
-var numbers = [];
- // create a array of numbers from 1 to 100
-for (var i = 0; i < 100; i++){
-numbers[i] =  i + 1;
-}
-
-
-for (var i = 0; i < numbers.length; i++){
+for (var i = 1; i <= 100; i++){
   // For numbers that are multiples of 3 and 5 print "FizzBuzz"
-  if (numbers[i] % 3 == 0 && numbers[i] % 5 == 0){
+  if (i % 3 == 0 && i % 5 == 0){
    console.log("FizzBuzz");
    // for multiples of 5 Buzz print "buzz"
- } else if (numbers[i] % 5 == 0){
-   console.log("buzz");
+ } else if (i % 5 == 0){
+   console.log("Buzz");
  }
  // for multiples of 3 print “Fizz”
- else if (numbers[i] % 3 == 0) {
+ else if (i % 3 == 0) {
    console.log("Fizz");
    // For numbers that are NOT multiples of 3 and 5 print the actual number
  }else{
-   console.log(numbers[i])
+   console.log(i)
  }
 }
